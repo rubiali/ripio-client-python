06deaf308c25bdeb5eff3a99dcc9542d2156cf4b
Felipe Avilan <juan.m@turing.com>
2022-12-26 20:59:45 -0500

    enable api key check per method

--- ripio/core.py
+++ ripio/core.py
@@ -3,34 +3,39 @@ import requests
 from ripio.exceptions.auth import UnathorizedClient
 
 class RipioClient(object):
-    
-    require_authorization = False
-    
     def __init__(self, api_key=None):
         self.session = requests.Session()
         self.api_key = ripio.api_key if api_key is None else api_key
-        self.check_api_key()
         
     def get(self, *args, **kwargs):
         request_args = args
         request_kwargs = kwargs
-        self.session.get(*request_args, **request_kwargs)
+        response = self.session.get(*request_args, **request_kwargs)
+        return response
+        
         
     def put(self, *args, **kwargs):
         request_args = args
         request_kwargs = kwargs
-        self.session.get(*request_args, **request_kwargs)
+        response =self.session.get(*request_args, **request_kwargs)
+        return response
+         
     
     def post(self, *args, **kwargs):
         request_args = args
         request_kwargs = kwargs
-        self.session.post(*request_args, **request_kwargs)
+        response = self.session.post(*request_args, **request_kwargs)
+        return response
         
     def delete(self, *args, **kwargs):
         request_args = args
         request_kwargs = kwargs
-        self.session.delete(*request_args, **request_kwargs)
+        response = self.session.delete(*request_args, **request_kwargs)
+        return response
         
-    def check_api_key(self):
-        if self.api_key is None and self.require_authorization:
-            raise UnathorizedClient("No credenatials were passed")
+    def check_api_key(func):
+        def checker(self, *args, **kwargs):
+            if self.api_key is None:
+                raise UnathorizedClient("No credenatials were passed")
+            func(self, *args, **kwargs)
+        return checker
--- ripio/trade/client.py
+++ ripio/trade/client.py
@@ -1,6 +1,13 @@
 from ripio.core import RipioClient
+import ripio
 
 class Client(RipioClient):
     def __init__(self, api_key = None):
         self.require_authorization = True
+        self.base_url = ripio.RIPIO_TRADE_BASE_URL
         super().__init__(api_key)
+        
+        
+    @RipioClient.check_api_key
+    def get_orders(self,*args, **kwargs):
+        response = self.get(f"{self.base_url}orders")
